519fcba1d961569a478a27922f88a16a7ec721b2
陳詩華 <chenshihua@chenshihuade-MacBook-Air.local>
2018-10-04 21:33:42 +0100

    Add tuna web font

--- src/Cocktails/globalStyles.js
+++ src/Cocktails/globalStyles.js
@@ -1,6 +1,11 @@
 import { injectGlobal } from 'styled-components'
 
 injectGlobal`
+
+    @font-face {
+        font-family: Tuna;
+        src: url("../fonts/tuna.otf");
+    }
     html, body {
         background-color: #fff;
         height: 100%;
